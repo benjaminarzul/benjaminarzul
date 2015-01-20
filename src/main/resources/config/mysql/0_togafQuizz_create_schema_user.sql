aab9c2bb4268219f35627b70d886764d27f26fa7
obarsot <olivier.barsot@softeam.fr>
2015-01-20 13:56:51 +0100

    Ajout d'un schéma togafQuizz pour MySQL avec un utilisateur togafQuizz

---
+++ src/main/resources/config/mysql/0_togafQuizz_create_schema_user.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS togafQuizz;
+
+CREATE DATABASE togafQuizz CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+GRANT ALL PRIVILEGES ON togafQuizz.* TO 'togafQuizz'@'localhost' IDENTIFIED BY 't0g@fQu1zz';
