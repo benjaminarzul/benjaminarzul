cb82336a5b9b09fe49e8ae1f746ae8fe8bf6d34d
obarsot <olivier.barsot@softeam.fr>
2015-01-29 11:49:01 +0100

    Développement (bouchon) de la page 'Mes réponses aux quizz'

--- src/main/webapp/scripts/app/quizzes/myanswers/myanswers.controller.js
+++ src/main/webapp/scripts/app/quizzes/myanswers/myanswers.controller.js
@@ -2,4 +2,9 @@
 
 angular.module('togafquizzApp')
     .controller('MyAnswersController', function ($scope) {
+    	$scope.quizzs = [
+    	                 {libelle: "quizz 1", dureeEnMinutes: "10"},
+    	                 {libelle: "quizz 2", dureeEnMinutes: "15"},
+    	                 {libelle: "quizz 3", dureeEnMinutes: "45"}
+    	                ];
     });
